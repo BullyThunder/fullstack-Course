37d8c610e0a0df2f9fdf4c8c3ca05b26c8877d60
Bully_Thunder <vamosbarca24-7@gmail.com>
2025-07-21 10:36:35 +0300

    save

--- backend/src/models/persons.js
+++ backend/src/models/persons.js
@@ -8,8 +8,19 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    required: true
+    required: true,
+    minlength: [8, 'Number must be at least 8 characters long'],
+    validate: {
+      validator: function(v) {
+        // Проверка: две части, разделённые дефисом
+        // Первая часть: 2 или 3 цифры
+        // Вторая часть: как минимум 5 цифр (чтобы длина >= 8)
+        return /^\d{2,3}-\d{5,}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    }
   }
-})
+});
+
 
 module.exports = mongoose.model('Person', personSchema)
